fd95a6793eee3ee6909873fb16bb2eb256756edb
Leonel <leonellrodriguez@gmail.com>
2022-09-19 22:31:39 -0500

    Autocomplete, input con search y llamados a AJAX a API Google

--- slider/app.js
+++ slider/app.js
@@ -87,8 +87,77 @@ class Slider {
   }
 }
 
+
+class Search {
+  static get(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    return new Promise((resolve, reject) => {
+      xhr.onreadystatechange = () => {
+        if(xhr.readyState == 4) {
+          if(xhr.status == 200) return resolve(JSON.parse(xhr.responseText));
+          // Algo salio mal
+          reject(xhr.status);
+        } 
+      }
+    });
+  }
+}
+
+class Autocomplete {
+  constructor(input_selector, base_url) {
+    this.search = this.search.bind(this);
+    this.input = document.querySelector(input_selector);
+    this.url = base_url;
+    this.value = "";
+    this.interval = null;
+    this.buildDataList();
+    this.bindEvents();
+  }
+
+  bindEvents() {  
+    this.input.addEventListener("keyup", () => {
+      if(this.value === this.input.value || this.input.value.length < 3) return;
+      if(this.interval) window.clearInterval(this.interval);
+
+      this.value = this.input.value;
+      this.interval = window.setTimeout(this.search, 500);
+    });
+  }
+
+  buildDataList() {
+    this.dataList = document.createElement("datalist");
+    this.dataList.id = "datalist-autocomplete";
+    document.querySelector("body").appendChild(this.dataList);
+    this.input.setAttribute("list", "datalist-autocomplete");
+  }
+
+  search() {
+    Search.get(this.url + this.value)
+      .then( results => this.build(results));
+  }
+
+  build(response) {
+    this.dataList.innerHTML = "";
+    response.items.forEach(item => {
+      let optionEl = document.createElement("option");
+      optionEl.value = item.volumeInfo.title;
+      if(item.volumeInfo.subtitle)
+        optionEl.innerHTML = item.volumeInfo.subtitle;
+
+      this.dataList.appendChild(optionEl);
+    })
+  }
+}
+
+
+
 (function(){
 
-  new Slider(".slider", true);
+  // new Slider(".slider", true);
+
+  const GooglBooksApiURL = "https://www.googleapis.com/books/v1/volumes?q=";
+  let autocomplete = new Autocomplete("#searcher", GooglBooksApiURL)
 
 })()
